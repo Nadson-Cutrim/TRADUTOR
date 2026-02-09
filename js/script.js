c2496b44a300b4d80234e5b3a35f478cd8dffd43
Nadson-Cutrim <nadsoncutrimjunior@aluno.uema.br>
2026-02-08 23:20:31 -0300

    ✨ Adicionando funcionalidade de ouvir tradução com reconhecimento de voz

--- js/script.js
+++ js/script.js
@@ -1,17 +1,30 @@
-let inputText = document.querySelector('#input-text');
+let inputText = document.querySelector("#input-text");
 let traducao = document.querySelector(".traducao");
 let idioma = document.querySelector(".idioma");
-async function traduzir(){
- //Endereço da API de tradução
-    let url = "https://api.mymemory.translated.net/get?q="
-    +inputText.value
-    +"&langpair=pt-BR|"+idioma.value;
-//resposta da API
-    let resposta = await fetch(url);
-//converter resposta para JSON
-    let dados = await resposta.json();
-    traducao.innerHTML = dados.responseData.translatedText;
-    console.log(dados);
+async function traduzir() {
+  //Endereço da API de tradução
+  let url =
+    "https://api.mymemory.translated.net/get?q=" +
+    inputText.value +
+    "&langpair=pt-BR|" +
+    idioma.value;
+  //resposta da API
+  let resposta = await fetch(url);
+  //converter resposta para JSON
+  let dados = await resposta.json();
+  traducao.innerHTML = dados.responseData.translatedText;
+  console.log(dados);
 }
 
+function ouvirVoz() {
+  const audio = window.webkitSpeechRecognition;
+  const recVoz = new audio();
+  recVoz.lang = "pt-BR";
 
+  recVoz.onresult = (event) => {
+    let fala = event.results[0][0].transcript;
+    inputText.textContent = fala;
+    traduzir();
+  };
+  recVoz.start();
+}
