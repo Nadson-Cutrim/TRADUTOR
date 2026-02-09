f8241a6eb92f445fc29924126cede4c5fa6aaa4a
Nadson-Cutrim <nadsoncutrimjunior@aluno.uema.br>
2026-02-08 21:31:49 -0300

    ✨ Função para tradução usando a API mymemory

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 let inputText = document.querySelector('#input-text');
 let traducao = document.querySelector(".traducao");
+let idioma = document.querySelector(".idioma");
 async function traduzir(){
  //Endereço da API de tradução
     let url = "https://api.mymemory.translated.net/get?q="
     +inputText.value
-    +"&langpair=pt-BR|en";
+    +"&langpair=pt-BR|"+idioma.value;
 //resposta da API
     let resposta = await fetch(url);
 //converter resposta para JSON
@@ -12,3 +13,5 @@ async function traduzir(){
     traducao.innerHTML = dados.responseData.translatedText;
     console.log(dados);
 }
+
+
